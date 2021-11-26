138f2c662484919c680074528822426549e1201a
Paviel Schertzer <paviel.schertzer@epita.fr>
2021-11-27 00:55:26 +0100

    RESEARCH - CUTTER : working wirth data folders - have an issue

--- research/cutterLabel/cuttterTest.py
+++ research/cutterLabel/cuttterTest.py
@@ -1,26 +1,45 @@
 from PIL import Image
+import dotenv
+import os
+import cv2
 
-im = Image.open(r"img.jpg")
-width, height = im.size
+dotenvFile = dotenv.find_dotenv()
+dotenv.load_dotenv(dotenvFile)
 
-label = "2 0.7403846153846154 0.7620192307692307 0.18389423076923078 0.25600961538461536"
-catego, x, y, dx, dy = label.split(' ')
-catego = int(catego)
-x = int(float(x) * width)
-y = int(float(y) * height)
-dx = int((float(dx) * width) / 2)
-dy = int((float(dy) * height) / 2)
+dataPath = os.getenv('DATAPATH')
 
-print(x)
-print(y)
-print(dx)
-print(dy)
-print(width)
-print(height)
+# TO DELETE
+#im = Image.open(r"img.jpg")
+#width, height = im.size
 
-left = x - dx
-right = x + dx
-top = y - dy #- because 0:0 on the left top corner
-bottom = y + dy
-im1 = im.crop((left, top, right, bottom))
-im1.save(str(catego) + ".cropped", "JPEG")
+
+# pourcours train test valid/ ...
+for partPath in ["train", 'test', "valid"] :
+    pathWork = os.path.join(dataPath, partPath)
+    # train test valid/ ... chaque image
+    for imageFile in os.listdir(os.path.join(pathWork, "images")) :
+        f = open(os.path.join(os.path.join(os.path.join(pathWork, "labels")), imageFile[:-4] + '.txt'))
+        # train test valid/ ... chaque image ... chaque label d'image
+        im = Image.open(os.path.join(os.path.join(os.path.join(pathWork, "images")), imageFile))
+        width, height = im.size
+        number=0
+        for label in f :
+            catego, x, y, dx, dy = label.split(' ')
+            catego = int(catego)
+            x = int(float(x) * width)
+            y = int(float(y) * height)
+            dx = int((float(dx) * width) / 2)
+            dy = int((float(dy) * height) / 2)
+
+            left = x - dx
+            right = x + dx
+            top = y - dy #- because 0:0 on the left top corner
+            bottom = y + dy
+
+            print(str((os.path.join(os.path.join(os.path.join(pathWork, "images")), imageFile))))
+            im1 = im.crop((left, top, right, bottom))
+            im1.save("transform/" + str(partPath) + "--" + str(catego) + "--" + str(imageFile) + str(number) + ".cropped", "JPEG")
+            number += 1
+
+        f.close()
+        im1.close()
