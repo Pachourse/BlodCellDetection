ad9269ee23c130ac4bff6265ee7ca6aae1f37bdf
paviel.schertzer <paviel.schertzer@epita.fr>
2021-12-15 10:12:28 +0100

    RESEARCH - CUTTER : FIX : error case handeling if label size 0 outside image

--- research/cutterLabel/cutterTest.py
+++ research/cutterLabel/cutterTest.py
@@ -25,7 +25,13 @@ for partPath in ["train", 'test', "valid"] :
         width, height = im.size
         number=0
         for label in f :
+            print("----")
             catego, x, y, dx, dy = label.split(' ')
+
+            if float(dx) == 0 or float(dy) == 0 : 
+                print("COUCOU")
+                continue
+
             catego = int(catego)
             x = int(float(x) * width)
             y = int(float(y) * height)
@@ -37,8 +43,12 @@ for partPath in ["train", 'test', "valid"] :
             top = y - dy #- because 0:0 on the left top corner
             bottom = y + dy
 
+
             print(str((os.path.join(os.path.join(os.path.join(pathWork, "images")), imageFile))))
+            print(label)
+            print(width, height, left, right, top, bottom)
             im1 = im.crop((left, top, right, bottom))
+            #im1 = im.crop((x, y, x + dx, x + dy))
             im1.save("transform/" + str(partPath) + "--" + str(catego) + "--" + str(imageFile) + str(number) + ".cropped", "JPEG")
             number += 1
 
